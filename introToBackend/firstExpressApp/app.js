d8faebd0cd66b0277a1171b919d2b1d4d850a4ff
jobeau <jordboudreau@gmail.com>
2018-01-10 09:05:23 +0000

    complete first express app

--- introToBackend/firstExpressApp/app.js
+++ introToBackend/firstExpressApp/app.js
@@ -1,1 +1,20 @@
-console.log("hello world");
+const express = require("express");
+const app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+
+app.get('/bye', function(req, res) {
+    res.send('Bye World!');
+});
+
+app.get('/dog', function(req, res) {
+    res.send('Meow');
+    console.log("Someone made a request to /dog");
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log('Server has started, Listening on ' + process.env.PORT +
+        ', IP ' + process.env.IP);
+});
